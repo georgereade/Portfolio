17614bb87ef77a3a0b45ee205cb197025a145ecb
georgereade <georgereade@hotmail.co.uk>
2023-08-18 14:26:30 +0100

    Added dotenv

--- projects/Hosted-To-Do-List/app.js
+++ projects/Hosted-To-Do-List/app.js
@@ -3,7 +3,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const _ = require("lodash")
+const _ = require("lodash");
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -140,6 +143,6 @@ app.get('/:customListName', function(req,res) {
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(PORT, function() {
+  console.log(`Server started on ${PORT}`);
 });
